e375d56ab3b8b09e61e0debdcc9239248b734661
ins-201 <ajay.gajra@inscripts.in>
2019-03-07 15:37:09 +0530

    Added CometChat

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
+import { CometChat } from '@cometchat-pro/chat'
 export const sayHelloToCompiler=function(compiler:string){
     console.log("Hello! "+compiler+",\nthis is typescript saying hi");
+    console.log(CometChat.getInstance().getApiKey());
 }
 sayHelloToCompiler("Jenkin from git");
